8e60bfdde2d113ddf567ca8bbed33692cc711301
briannesharp <64488627+briannesharp@users.noreply.github.com>
2026-03-11 14:11:59 -0400

    initial commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'darley-v1';
+const URLS  = ['./index.html', './'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      caches.open(CACHE).then(c => c.put(e.request, r.clone()));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
